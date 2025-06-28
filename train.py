3b2b719f862100220d814b9ed9b7ac93cb636549
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2025-06-28 16:50:19 -0400

    Update train.py
    added some comments
    added weight decay
    added cross validation function

--- train.py
+++ train.py
@@ -37,19 +37,26 @@ from utils import *
 
 def main():
 
+    # parses all passed arguments
     args = cfg.parse_args()
 
+    # sets the random seed for reproducibility
+    # I changed the default to 22 because i like that number
     seed = args.seed
     set_seed(seed)
 
     GPUdevice = torch.device('cuda', args.gpu_device)
 
+    # get_network defined in utils.py 
+    # returns an instance of the network (sam)
     net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution = args.distributed)
     if args.pretrain:
         weights = torch.load(args.pretrain)
         net.load_state_dict(weights,strict=False)
 
-    optimizer = optim.Adam(net.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+    # sets up optimizer and learning rate scheduler
+    optimizer = optim.Adam(net.parameters(), lr=args.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5, amsgrad=False)
+    # scheduler doesnt seem to be used????
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5) #learning rate decay
 
     '''load pretrained model'''
@@ -74,6 +81,7 @@ def main():
     logger = create_logger(args.path_helper['log_path'])
     logger.info(args)
 
+    # test should be called val instead
     nice_train_loader, nice_test_loader = get_dataloader(args)
 
     '''checkpoint path and tensorboard'''
@@ -97,52 +105,68 @@ def main():
     best_tol = 1e4
     best_dice = 0.0
 
-    for epoch in range(settings.EPOCH):
+    """ cross-validation """
+    if args.cross_validate:
+        print(f"\n=== Running 5-Fold Cross Validation ===")
+        for fold in range(5):
+            args.current_fold = fold
+            print(f"\n=== Fold {fold + 1} ===")
 
-        if epoch < 5:
-            if args.dataset != 'REFUGE':
-                tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-                logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
-            else:
-                tol, (eiou_cup, eiou_disc, edice_cup, edice_disc) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-                logger.info(f'Total score: {tol}, IOU_CUP: {eiou_cup}, IOU_DISC: {eiou_disc}, DICE_CUP: {edice_cup}, DICE_DISC: {edice_disc} || @ epoch {epoch}.')
+            nice_train_loader, nice_test_loader = get_dataloader(args)
 
-        net.train()
-        time_start = time.time()
-        loss = function.train_sam(args, net, optimizer, nice_train_loader, epoch, writer, vis = args.vis)
-        logger.info(f'Train loss: {loss} || @ epoch {epoch}.')
-        time_end = time.time()
-        print('time_for_training ', time_end - time_start)
+            net = get_network(args, args.net, use_gpu=args.gpu, gpu_device=GPUdevice, distribution=args.distributed)
+            optimizer = optim.Adam(net.parameters(), lr=args.lr, weight_decay=1e-5)
+            scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
 
-        net.eval()
-        if epoch and epoch % args.val_freq == 0 or epoch == settings.EPOCH-1:
-            if args.dataset != 'REFUGE':
-                tol, (eiou, edice) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-                logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
-            else:
-                tol, (eiou_cup, eiou_disc, edice_cup, edice_disc) = function.validation_sam(args, nice_test_loader, epoch, net, writer)
-                logger.info(f'Total score: {tol}, IOU_CUP: {eiou_cup}, IOU_DISC: {eiou_disc}, DICE_CUP: {edice_cup}, DICE_DISC: {edice_disc} || @ epoch {epoch}.')
+            best_dice = 0.0
 
-            if args.distributed != 'none':
-                sd = net.module.state_dict()
-            else:
-                sd = net.state_dict()
+            for epoch in range(settings.EPOCH):
+                if epoch < 5:
+                    validate_epoch(args, epoch, net, nice_test_loader, writer, logger)
 
-            if edice > best_dice:
-                best_tol = tol
-                is_best = True
+                train_epoch(args, epoch, net, optimizer, nice_train_loader, writer, logger, GPUdevice)
+                scheduler.step()
 
-                save_checkpoint({
-                'epoch': epoch + 1,
-                'model': args.net,
-                'state_dict': sd,
-                'optimizer': optimizer.state_dict(),
-                'best_tol': best_dice,
-                'path_helper': args.path_helper,
-            }, is_best, args.path_helper['ckpt_path'], filename="best_dice_checkpoint.pth")
-                print(f'New best DICE score: {edice} at epoch {epoch}. Saved best model at {args.path_helper["ckpt_path"]}.')
-            else:
-                is_best = False
+                if epoch % args.val_freq == 0 or epoch == settings.EPOCH - 1:
+                    tol, edice = validate_epoch(args, epoch, net, nice_test_loader, writer, logger)
+
+                    if edice > best_dice:
+                        best_dice = edice
+                        save_checkpoint({
+                            'epoch': epoch + 1,
+                            'model': args.net,
+                            'state_dict': net.state_dict(),
+                            'optimizer': optimizer.state_dict(),
+                            'best_tol': best_dice,
+                            'path_helper': args.path_helper,
+                        }, True, args.path_helper['ckpt_path'], filename=f"best_fold{fold}_dice_checkpoint.pth")
+                        print(f'New best DICE score: {edice} at epoch {epoch}. Saved best model at {args.path_helper["ckpt_path"]}.')
+    
+    else:
+        args.current_fold = args.fold
+        nice_train_loader, nice_test_loader = get_dataloader(args)
+
+        for epoch in range(settings.EPOCH):
+            if epoch < 5:
+                validate_epoch(args, epoch, net, nice_test_loader, writer, logger)
+
+            train_epoch(args, epoch, net, optimizer, nice_train_loader, writer, logger, GPUdevice)
+            scheduler.step()
+
+            if epoch % args.val_freq == 0 or epoch == settings.EPOCH - 1:
+                tol, edice = validate_epoch(args, epoch, net, nice_test_loader, writer, logger)
+
+                if edice > best_dice:
+                    best_dice = edice
+                    save_checkpoint({
+                        'epoch': epoch + 1,
+                        'model': args.net,
+                        'state_dict': net.state_dict(),
+                        'optimizer': optimizer.state_dict(),
+                        'best_tol': best_dice,
+                        'path_helper': args.path_helper,
+                    }, True, args.path_helper['ckpt_path'], filename="best_dice_checkpoint.pth")
+                    print(f'New best DICE score: {edice} at epoch {epoch}. Saved best model at {args.path_helper["ckpt_path"]}.')
 
     writer.close()
 
