33fe00ae11134058796d40dd3f12109077231a47
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2025-07-10 11:20:42 -0400

    Create 03-MedSAMBox.sh

---
+++ experiments/03-MedSAMBox.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#SBATCH --time=01:00:00 
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem=6G
+#SBATCH --account=def-banire
+#SBATCH --job-name=MedSAMBox
+#SBATCH --output=/lustre07/scratch/jieying/Medical-SAM-Adapter/out/03-MedSAMBox.out
+
+# Load modules
+module load StdEnv/2023 gcc/12.3 cuda/12.2 opencv/4.10.0 
+
+# Activate virtual environment
+source ~/envs/sam-adapt/bin/activate
+
+# Unzip data in job
+mkdir $SLURM_TMPDIR/data
+tar xf /lustre07/scratch/jieying/Medical-SAM-Adapter/data/train.tar -C $SLURM_TMPDIR/data
+
+python /lustre07/scratch/jieying/Medical-SAM-Adapter/train.py \
+    -exp_name 03-MedSAMBox-ZP \
+    -vis 10 \
+    -val_freq 1 \
+    -b 5 -dataset oo \
+    -sam_ckpt /lustre07/scratch/jieying/Medical-SAM-Adapter/checkpoint/medsam/medsam_vit_b.pth \
+    -d $SLURM_TMPDIR/data/train \
+    -l ZP
+
+python /lustre07/scratch/jieying/Medical-SAM-Adapter/train.py \
+    -exp_name 03-MedSAMBox-PVS \
+    -vis 10 \
+    -val_freq 1 \
+    -b 5 -dataset oo \
+    -sam_ckpt /lustre07/scratch/jieying/Medical-SAM-Adapter/checkpoint/medsam/medsam_vit_b.pth \
+    -d $SLURM_TMPDIR/data/train \
+    -l PVS
+
+python /lustre07/scratch/jieying/Medical-SAM-Adapter/train.py \
+    -exp_name 03-MedSAMBox-OO \
+    -vis 10 \
+    -val_freq 1 \
+    -b 5 -dataset oo \
+    -sam_ckpt /lustre07/scratch/jieying/Medical-SAM-Adapter/checkpoint/medsam/medsam_vit_b.pth \
+    -d $SLURM_TMPDIR/data/train \
+    -l OO
+
+python /lustre07/scratch/jieying/Medical-SAM-Adapter/train.py \
+    -exp_name 03-MedSAMBox-PB \
+    -vis 10 \
+    -val_freq 1 \
+    -b 5 -dataset oo \
+    -sam_ckpt /lustre07/scratch/jieying/Medical-SAM-Adapter/checkpoint/medsam/medsam_vit_b.pth \
+    -d $SLURM_TMPDIR/data/train \
+    -l PB
