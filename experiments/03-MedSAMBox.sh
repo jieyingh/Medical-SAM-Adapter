36777e2c55c900288741dbf69443fb73af086780
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2025-07-10 11:22:16 -0400

    Update 03-MedSAMBox.sh
    lower ressources

--- experiments/03-MedSAMBox.sh
+++ experiments/03-MedSAMBox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=01:00:00 
 #SBATCH --gpus-per-node=1
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=6G
 #SBATCH --account=def-banire
 #SBATCH --job-name=MedSAMBox
